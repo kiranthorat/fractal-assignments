9ea7756cf750bf00a132696d3e9ca14e31805056
kiranthorat <thoratkiran776@gmail.com>
2025-08-05 12:32:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const obj  = {
+  name: 'John',
+  age: 30,
+  city: 'New York'
+};
+
+console.log(obj);
+console.log(JSON.stringify(obj, null, 2));
---
+++ script.js
@@ -0,0 +1,5 @@
+const pie=3.14159;
+const square = (x) => x * x;
+
+const circleArea = (radius) => pie * square(radius);    
+console.log(circleArea(5)); // Outputs: 78.53975
