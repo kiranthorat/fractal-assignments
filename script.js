b2b469a3d3a2a73e0ae01d85b9a178f4a12a7c27
kiranthorat <thoratkiran776@gmail.com>
2025-08-05 12:43:44 +0530

    second commint

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const square = (x) => x * x;
 
 const circleArea = (radius) => pie * square(radius);    
 console.log(circleArea(5)); // Outputs: 78.53975
+
+console.log(JSON.parse(JSON.stringify(obj)));
